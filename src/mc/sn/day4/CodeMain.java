ef4866e55cc086cec16274b5b2b16d7586483757
Hyuk Jin Choi <62084630+sc62070@users.noreply.github.com>
2021-09-16 14:36:41 +0900

    5교시 소수 판별소수

---
+++ src/mc/sn/day4/CodeMain.java
@@ -0,0 +1,11 @@
+package mc.sn.day4;
+
+public class CodeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PracticeCode pc = new PracticeCode();
+		boolean flag = pc.isPrimeNumber(10);
+		System.out.println(flag);
+	}
+}
---
+++ src/mc/sn/day4/PracticeCode.java
@@ -0,0 +1,10 @@
+package mc.sn.day4;
+
+public class PracticeCode {
+	public boolean isPrimeNumber(int number) {
+		//소수 여부 판단
+		boolean result = false;
+		
+		return result;
+	}
+}	
