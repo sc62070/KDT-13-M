5a159f82ff4e92bc647c4514e0af498d96abd75c
Hyuk Jin Choi <62084630+sc62070@users.noreply.github.com>
2021-09-17 10:43:36 +0900

    window

--- src/mc/sn/step1/ReviewMain.java
+++ src/mc/sn/step1/ReviewMain.java
@@ -7,13 +7,17 @@ public class ReviewMain {
 		// 1 ~ 10까지 반복문을 이용하여 콘솔에 출력하는 코드를 작성하시오
 		for(int i=0;i<10;i++) {
 			// 콘솔 출력하는 코드 작성
-			
+			int num = i+1;
+		System.out.println(num);	
 		}
 		
 		//printNumber를 호출하는 코드를 작성하시오
 		//1. 현재 클래스의 인스턴스를 생성한다.
+		ReviewMain pm = new ReviewMain();
+		pm.printNumber();
 		
 		//2. 생성인스턴스의 변수를 이용하여 printNumber메소드를 호출한다.
+		
 	}
 	
 	
@@ -22,7 +26,9 @@ public class ReviewMain {
 		// 데이터 타입이 int이고 식별자가 number인 변수에 기본값을 할당한다
 		int number = 0;
 		// 10번 반복하는 for문을 정의하시오
-		
+		for(int i=0;i<10;i++) {
+			int val = i+1;
+		}
 		{
 			//for의 정의를 이용하여 1~10까지의 수를 가지는 number라는 변수를 구성하시오
 			
